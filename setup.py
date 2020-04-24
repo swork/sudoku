d42c5977f067773b81293c958bad7d510dd7c116
Steve Work <steve@work.renlabs.com>
2020-04-24 11:47:31 -0700

    bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sudoku",
-    version="0.0.1",
+    version="1.0.0",
     packages=[ 'renlabs.sudoku' ],
     author="Steve Work",
     author_email="steve@work.renlabs.com",
