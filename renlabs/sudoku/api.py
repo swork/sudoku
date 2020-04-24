6e654db9bf21442030af93bd03438085aa1ab3c8
Steve Work <steve@work.renlabs.com>
2020-04-23 17:40:03 -0700

    Split / into separate functions for GET and POST

--- renlabs/sudoku/api.py
+++ renlabs/sudoku/api.py
@@ -2,38 +2,45 @@ import flask
 from . import Sudoku
 from textwrap import wrap, dedent
 
-application = flask.Flask('sudoKu')  # __name__.split('.',1)[0])  # "sudoku"
+import logging, os
+logger = logging.getLogger(__name__)
+logger.setLevel(int(os.environ.get('LEVEL', logging.DEBUG)))
 
-@application.route('/sudoku', methods=['GET', 'POST'])
-def index_solve():
-    if flask.request.method == 'GET':
+application = flask.Flask('sudoku')  # __name__.split('.',1)[0])  # "sudoku"
+
+@application.route('/', methods=['GET'])
+def inform():
+    return flask.jsonify(
+        status=200,
+        message='OK',
+        body = {
+            "sudoku": "welcome",
+            "info": wrap(
+                dedent("""\
+                Post a JSON list of nine lists of integers,
+                each a row of a sudoku puzzle,
+                with zeros for the empty squares.
+                Get back a solution, maybe one of several."""),
+                drop_whitespace=True)})
+
+@application.route('/', methods=['POST'])
+def solve():
+    s = Sudoku()
+    input = flask.request.get_json(force=True)
+    logger.debug(f'input:{input}')
+    s.inputList(input)
+    if s.solution:
         return flask.jsonify(
             status=200,
             message='OK',
-            body = {
-                "sudoku": "welcome",
-                "info": wrap(dedent("""\
-                    Post a JSON list of nine lists of integers,
-                    each a row of a sudoku puzzle,
-                    with zeros for the empty squares.
-                    Get back a solution, maybe one of several."""),
-                             drop_whitespace=True)})
-    elif flask.request.method == 'POST':
-        s = Sudoku()
-        input = flask.request.get_json(force=True)
-        s.inputList(input)
-        if s.solution:
-            return flask.jsonify(
-                status=200,
-                message='OK',
-                body={
-                    "input": input,
-                    "solution": s.solution})
-        return flask.jsonify(
-            status=204,
-            message='No solution',
             body={
                 "input": input,
-                "nosolution": "No solution is possible."
-            })
+                "solution": s.solution})
+    return flask.jsonify(
+        status=204,
+        message='No solution',
+        body={
+            "input": input,
+            "nosolution": "No solution is possible."
+        })
 
